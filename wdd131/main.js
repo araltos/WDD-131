52c321d8fb202f6d85ee1a5a62307248779eb8b6
SantiagoJ <leonardo.jara.saltos@gmail.com>
2024-10-28 09:50:23 -0600

    undo the update

--- wdd131/main.js
+++ wdd131/main.js
@@ -1,25 +1,6 @@
 let tasks = [];
 
-function parseURLParams() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const taskCount = urlParams.get('taskCount');
-    
-    if (taskCount) {
-        for (let i = 0; i < taskCount; i++) {
-            const taskKey = `task_${i}`;
-            const taskValue = urlParams.get(taskKey);
-            if (taskValue) {
-                const completed = taskValue.startsWith("[X]");
-                const description = taskValue.replace(/^\[X\] |\[ \]/, "").trim();
-                tasks.push({ id: i, description, completed });
-            }
-        }
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-}
-
 window.onload = function() {
-    parseURLParams();
     const storedTasks = JSON.parse(localStorage.getItem('tasks'));
     if (storedTasks) {
         tasks = storedTasks;
@@ -66,7 +47,7 @@ function displayTasks() {
     if (tasks.length === 0) {
         taskContainer.innerHTML = "<li>No tasks added yet!</li>";
     }
-
+    
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
@@ -83,10 +64,9 @@ window.deleteTask = function(id) {
 };
 
 function generateQRCode() {
-    const baseURL = window.location.href.split('?')[0];
-    const params = new URLSearchParams();
+    const baseURL = "https://example.com/todo";
 
-    params.append('taskCount', tasks.length);
+    const params = new URLSearchParams();
     tasks.forEach(task => {
         const taskKey = `task_${task.id}`;
         const taskValue = `${task.completed ? "[X]" : "[ ]"} ${task.description}`;
